feaed2fefb3b39ba935db4355bb0d6a4e6f71d46
Julien Nyambal <julien.nyambal@gmail.com>
2025-03-10 08:30:38 +0000

    adding new files: llama cpp

---
+++ llama.cpp/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/mtn_chatbot/llama.cpp
+# Build directory: /home/ubuntu/mtn_chatbot/llama.cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("ggml")
+subdirs("src")
+subdirs("common")
+subdirs("tests")
+subdirs("examples")
+subdirs("pocs")
